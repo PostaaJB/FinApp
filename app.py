d8ebe4224fb725f72e13726bff80dc7c41a9c6a8
PostaaJB <edoardo.torelli00@gmail.com>
2026-09-20 11:47:00 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,8 +60,8 @@ def pac_modal():
 col_logo, col_titolo, col_bot = st.columns([0.2, 0.6, 0.2])
 
 with col_logo:
-    # Mostra la tua immagine calcolatrice caricata su GitHub
-    st.image("logo.png", use_container_width=True)
+    # Usiamo il link "Raw" (diretto) di GitHub bypassando il caricamento locale
+    st.image("https://raw.githubusercontent.com/postaajb/finapp/main/logo.png", use_container_width=True)
     
 with col_titolo:
     st.title("FinApp")
